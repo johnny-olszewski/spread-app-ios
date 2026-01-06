cc8ab9c3069fc492351a6706e83493dcff573450
johnny <johnnyo.dev@gmail.com>
2026-01-06 13:41:47 -0800

    Initial Commit

---
+++ Spread/SpreadApp.swift
@@ -0,0 +1,32 @@
+//
+//  SpreadApp.swift
+//  Spread
+//
+//  Created by Johnny O on 1/6/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SpreadApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SpreadTests/SpreadTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpreadTests.swift
+//  SpreadTests
+//
+//  Created by Johnny O on 1/6/26.
+//
+
+import Testing
+@testable import Spread
+
+struct SpreadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
