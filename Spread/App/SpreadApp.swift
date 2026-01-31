73297af896da06ef7e2e61ffeaab76d597cdf0a6
johnny <johnnyo.dev@gmail.com>
2026-01-31 08:41:12 -0800

    [SPRD-94][7/n] log Supabase host and fix test config

--- Spread/App/SpreadApp.swift
+++ Spread/App/SpreadApp.swift
@@ -1,10 +1,13 @@
+import OSLog
 import SwiftUI
 
 @main
 struct SpreadApp: App {
+    private static let logger = Logger(subsystem: "dev.johnnyo.Spread", category: "App")
     private let container: DependencyContainer
 
     init() {
+        logSupabaseConfiguration()
         do {
             container = try DependencyContainer.make(for: .current)
         } catch {
@@ -16,5 +19,16 @@ struct SpreadApp: App {
         WindowGroup {
             ContentView(container: container)
         }
+    }
+
+    private func logSupabaseConfiguration() {
+        let host = SupabaseConfiguration.url.host ?? SupabaseConfiguration.url.absoluteString
+        var overrideSource = SupabaseConfiguration.explicitOverrideSourceDescription ?? "None"
+        #if DEBUG
+        if let runtimeOverride = SupabaseConfiguration.runtimeOverrideDescription {
+            overrideSource = runtimeOverride
+        }
+        #endif
+        SpreadApp.logger.info("Supabase host: \(host, privacy: .public) (override: \(overrideSource, privacy: .public))")
     }
 }
